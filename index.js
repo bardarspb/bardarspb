0f5d04cbb2ca12fa3391da8f0908a73f3c431d57
bardarspb <102213136+bardarspb@users.noreply.github.com>
2022-03-23 10:55:22 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+echo "# bardarspb" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/bardarspb/bardarspb.git
+git push -u origin main
